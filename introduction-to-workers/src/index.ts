deb079bc4302f308640a5e9277010769d1f54245
siggu <siggu100@gmail.com>
2026-06-15 14:37:42 +0900

    update: 카운트를 KV에 저장하도록 변경하고 경로 라우팅 추가

--- introduction-to-workers/src/index.ts
+++ introduction-to-workers/src/index.ts
@@ -1,8 +1,12 @@
-let count = 0;
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		count = count + 1;
-		return new Response(`Count is ${count}`);
+		const url = new URL(request.url);
+		console.log(request.cf?.country);
+		if (url.pathname === '/') {
+			const count = Number((await env.DB.get('count')) ?? 0);
+			await env.DB.put('count', `${count + 1}`);
+			return new Response(`Count: ${count + 1}`);
+		}
+		return new Response(null, { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
