4d6832a669f9e9f2b4fe7feb973e2467e0363f42
siggu <siggu100@gmail.com>
2026-06-19 15:04:27 +0900

    feat: Durable Object 카운터 증가 기능과 경로 라우팅 추가

--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -1,14 +1,24 @@
 import { DurableObject } from 'cloudflare:workers';
 
 export class DurablePractice extends DurableObject<Env> {
-	ping() {
-		return 'pong';
+	count = 0;
+
+	increase() {
+		this.count++;
+		return `count is ${this.count}`;
 	}
 }
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const stub = env.DurablePractice.getByName('default');
-		return new Response(await stub.ping());
+		const { pathname } = new URL(request.url);
+
+		if (pathname === '/') {
+			const dp = env.DurablePractice.getByName('default');
+			return new Response(await dp.increase());
+		}
+		return new Response(null, {
+			status: 404,
+		});
 	},
 } satisfies ExportedHandler<Env>;
