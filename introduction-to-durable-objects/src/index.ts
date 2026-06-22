3415191103d0dd25ddfd8d7651ca29683b9be647
siggu <siggu100@gmail.com>
2026-06-22 16:22:16 +0900

    feat: /ws 경로에서 WebSocket 연결 수락 및 메시지 핸들러 추가

--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -2,18 +2,35 @@ import { DurableObject } from 'cloudflare:workers';
 
 export class DurablePractice extends DurableObject<Env> {
 	fetch(request: Request) {
-		return new Response(`Hello`);
+		const url = new URL(request.url);
+		const nickname = url.searchParams.get('nickname') ?? 'anonymous';
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+
+		this.ctx.acceptWebSocket(server);
+
+		return new Response(null, { status: 101, webSocket: client });
+	}
+
+	webSocketMessage(ws: WebSocket, message: string) {
+		console.log(message);
+	}
+
+	webSocketClose(ws: WebSocket) {
+		console.log('WebSocket closed');
 	}
 }
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const { pathname, searchParams } = new URL(request.url);
-		const roomId = searchParams.get('roomId') ?? 'public';
-		const upgrade = request.headers.get('Upgrade');
-		if (upgrade) {
-			const dp = env.DurablePractice.getByName(roomId);
-			return dp.fetch(request);
+		if (pathname === '/ws') {
+			const roomId = searchParams.get('roomId') ?? 'public';
+			const upgrade = request.headers.get('Upgrade');
+			if (upgrade) {
+				const dp = env.DurablePractice.getByName(roomId);
+				return dp.fetch(request);
+			}
 		}
 		return new Response(null, {
 			status: 404,
