488696d27165414ba228915bea9b4b2ee91654ca
siggu <siggu100@gmail.com>
2026-06-22 15:03:57 +0900

    feat: roomId별 WebSocket 업그레이드 요청을 Durable Object로 라우팅

--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -1,48 +1,19 @@
 import { DurableObject } from 'cloudflare:workers';
 
 export class DurablePractice extends DurableObject<Env> {
-	sql: SqlStorage;
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
-
-		this.sql = ctx.storage.sql;
-
-		ctx.storage.sql.exec(`
-			CREATE TABLE IF NOT EXISTS counts (
-				id INTEGER PRIMARY KEY AUTOINCREMENT,
-				total INTEGER
-			);
-		`);
-
-		ctx.storage.sql.exec(`
-			INSERT OR IGNORE INTO counts (id, total) VALUES (1, 0);
-		`);
-	}
-
-	async increase() {
-		const { total } = this.sql.exec(`UPDATE counts SET total = total + 1 WHERE id = 1 RETURNING total;`).one() as { total: number };
-		if (total >= 30) {
-			const currentAlarm = await this.ctx.storage.getAlarm();
-			console.log('alarm', currentAlarm);
-			if (currentAlarm === null) {
-				this.ctx.storage.setAlarm(Date.now() + 5_000);
-			}
-		}
-		return `count is ${total}`;
-	}
-
-	alarm() {
-		this.sql.exec(`UPDATE counts SET total = 0 WHERE id = 1`);
+	fetch(request: Request) {
+		return new Response(`Hello`);
 	}
 }
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const { pathname, searchParams } = new URL(request.url);
-		const nickname = searchParams.get('nickname') || 'anonymous';
-		if (pathname === '/') {
-			const dp = env.DurablePractice.getByName(nickname);
-			return new Response(await dp.increase());
+		const roomId = searchParams.get('roomId') ?? 'public';
+		const upgrade = request.headers.get('Upgrade');
+		if (upgrade) {
+			const dp = env.DurablePractice.getByName(roomId);
+			return dp.fetch(request);
 		}
 		return new Response(null, {
 			status: 404,
